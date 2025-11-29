356099516faa972a43da2874d388b882d36637f2
kl2400030612 <2400030612@kluniversity.in>
2025-11-29 20:04:14 +0530

    Bugs and Changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { initializeData } from './services/initializeData.js';
+import './index.css';
 
 initializeData(); // <--- IMPORTANT
 
